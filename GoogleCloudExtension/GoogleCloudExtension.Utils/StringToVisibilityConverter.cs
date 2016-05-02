587366b43efcdb8b976d027c55af03ca2522b20c
Sergey Pugachev <spugachev@gmail.com>
2016-05-02 07:19:26 +0000

    Improved visual styling os the controls

---
+++ GoogleCloudExtension/GoogleCloudExtension.Utils/StringToVisibilityConverter.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2015 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace GoogleCloudExtension.Utils
+{
+    /// <summary>
+    /// Converts a string value into a Visibility enum value. 
+    /// Any non white space mean Visible and null or white space means Collapsed.
+    /// Note: Only Convert is implemented, so this is not a bidirectional converter, do not use on TwoWay bindings.
+    /// </summary>
+    public class StringToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        /// <summary>
+        /// Whether logging the value being converted is enabled.
+        /// </summary>
+        public bool LoggingEnabled { get; set; }
+
+        /// <summary>
+        /// The prefix string to use for the log messages, useful for finding the entries in the 
+        /// Output window.
+        /// </summary>
+        public string LoggingPrefix { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = value as string;
+
+            var result = !string.IsNullOrWhiteSpace(val) ?
+            Visibility.Visible : Visibility.Collapsed;
+
+            if (LoggingEnabled)
+            {
+                Debug.WriteLine($"{nameof(StringToVisibilityConverter)}: {LoggingPrefix} converting {value} to {result}");
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        //MarkupExtension implementation allows to use converter
+        //directly in markup without creating static resource
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
--- GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/PubSub/Common/ExtensionBrushes.cs
+++ GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/PubSub/Common/ExtensionBrushes.cs
@@ -16,5 +16,27 @@ namespace GoogleCloudExtension.CloudExplorerSources.PubSub.Common
         public static object TextBoxBackgroundFocused => new ThemeResourceKey(CommonControls, "TextBoxBackgroundFocused", ThemeResourceKeyType.BackgroundBrush);
         public static object TextBoxBorderFocused => new ThemeResourceKey(CommonControls, "TextBoxBorderFocused", ThemeResourceKeyType.BackgroundBrush);
         public static object TextBoxTextFocused => new ThemeResourceKey(CommonControls, "TextBoxTextFocused", ThemeResourceKeyType.BackgroundBrush);
+
+        public static object CheckBoxBackground => new ThemeResourceKey(CommonControls, "CheckBoxBackground", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBackgroundPressed => new ThemeResourceKey(CommonControls, "CheckBoxBackgroundPressed", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBackgroundHover => new ThemeResourceKey(CommonControls, "CheckBoxBackgroundHover", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxText => new ThemeResourceKey(CommonControls, "CheckBoxText", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxTextHover => new ThemeResourceKey(CommonControls, "CheckBoxTextHover", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxTextPressed => new ThemeResourceKey(CommonControls, "CheckBoxTextPressed", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxTextDisabled => new ThemeResourceKey(CommonControls, "CheckBoxTextDisabled", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxTextFocused => new ThemeResourceKey(CommonControls, "CheckBoxTextFocused", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBackgroundDisabled => new ThemeResourceKey(CommonControls, "CheckBoxBackgroundDisabled", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBackgroundFocused => new ThemeResourceKey(CommonControls, "CheckBoxBackgroundFocused", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBorder => new ThemeResourceKey(CommonControls, "CheckBoxBorder", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBorderHover => new ThemeResourceKey(CommonControls, "CheckBoxBorderHover", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBorderPressed => new ThemeResourceKey(CommonControls, "CheckBoxBorderPressed", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBorderDisabled => new ThemeResourceKey(CommonControls, "CheckBoxBorderDisabled", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxBorderFocused => new ThemeResourceKey(CommonControls, "CheckBoxBorderFocused", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxGlyph => new ThemeResourceKey(CommonControls, "CheckBoxGlyph", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxGlyphHover => new ThemeResourceKey(CommonControls, "CheckBoxGlyphHover", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxGlyphPressed => new ThemeResourceKey(CommonControls, "CheckBoxGlyphPressed", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxGlyphDisabled => new ThemeResourceKey(CommonControls, "CheckBoxGlyphDisabled", ThemeResourceKeyType.BackgroundBrush);
+        public static object CheckBoxGlyphFocused => new ThemeResourceKey(CommonControls, "CheckBoxGlyphFocused", ThemeResourceKeyType.BackgroundBrush);
+
     }
 }
